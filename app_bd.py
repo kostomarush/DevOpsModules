a1092e023e897613b345ea1c25259436e1e3cfae
kuber <kuber@example.com>
2025-03-25 12:50:39 +0000

    qwe

---
+++ app_bd.py
@@ -0,0 +1,30 @@
+import psycopg2
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Подключение к PostgreSQL
+def get_db_connection():
+    return psycopg2.connect(
+        dbname="test_db",
+        user="postgres",
+        password="P@ssw0rd",
+        host="postgres",
+        port="5432"
+    )
+
+# Маршрут для получения всех данных из всех таблиц
+@app.route('/tables')
+def get_all_tables():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM data;")
+    tables = cur.fetchall()
+
+    
+    cur.close()
+    conn.close()
+    return jsonify([{"id": t[0], "data": t[1]} for t in tables])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5050, debug=True)
