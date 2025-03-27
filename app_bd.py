66e563797a8cb2e11d2ffa202644d7cf08ff4d62
kuber <kuber@example.com>
2025-03-27 10:19:03 +0000

    prod_v2

--- app_bd.py
+++ app_bd.py
@@ -27,7 +27,7 @@ def get_all_tables():
 
 @app.route('/test')
 def get_test():
-    return "<h1>Test Page - All Tables</h1>"
+    return "<h1>Kuber v.2</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5050, debug=True)
