395dfdd12e3f5b7534a23da48b4421c0f8b500b0
kuber <kuber@example.com>
2025-03-28 09:11:02 +0000

    test

--- app_bd.py
+++ app_bd.py
@@ -27,7 +27,7 @@ def get_all_tables():
 
 @app.route('/test')
 def get_test():
-    return "<h1>Kuber v.6</h1>"
+    return "<h1>Kubernetes v.6</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5050, debug=True)
